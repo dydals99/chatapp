a1df5e5401461fe4b2403dc92df12ba38499a5fa
dydals99 <whdydals8604@gmail.com>
2024-06-02 17:38:01 +0900

    채팅 메세지 전송 및 저장

--- chatapp/src/main/java/com/edu/chatapp/ChatController.java
+++ chatapp/src/main/java/com/edu/chatapp/ChatController.java
@@ -16,23 +16,30 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @RequiredArgsConstructor
-@RestController
+@RestController // 데이터 리턴 서버
 public class ChatController {
 
 	private final ChatRepository chatRepository;
 	
+	// 귓속말 할때 사용하면 되요!!
 	@CrossOrigin
-	@GetMapping(value = "/sender/{sender}/receiver/{receiver}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
 		return chatRepository.mFindBySender(sender, receiver)
 				.subscribeOn(Schedulers.boundedElastic());
 	}
 	
 	@CrossOrigin
-	@PostMapping("/chat")
-	public Mono<Chat> setMsg(@RequestBody Chat chat){
-		chat.setCreateAt(LocalDateTime.now());
-		return chatRepository.save(chat);
+	@GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+		return chatRepository.mFindByRoomNum(roomNum)
+				.subscribeOn(Schedulers.boundedElastic());
 	}
 	
+	@CrossOrigin
+	@PostMapping("/chat") 
+	public Mono<Chat> setMsg(@RequestBody Chat chat){
+		chat.setCreatedAt(LocalDateTime.now());
+		return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환 (MessageConverter)
+	}
 }
