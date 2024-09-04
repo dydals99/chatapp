86ce47053edcc16d121438877da2e8176dc45dda
dydals99 <whdydals8604@gmail.com>
2024-09-04 23:24:38 +0900

    commit test

--- chatapp/src/main/java/com/edu/chatapp/Chat.java
+++ chatapp/src/main/java/com/edu/chatapp/Chat.java
@@ -15,7 +15,7 @@ public class Chat {
 	@Id
 	private String id;
 	private String msg;
-	private String sender; // 보내는 사람
+	private String sender; // 보내는 사람  
 	private String receiver; // 받는 사람 (귓속말)
 	private Integer roomNum; // 방 번호
 	
